74403ff18d66512d00bbc33e7b18c314702da2c6
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2021-10-14 14:41:26 +0200

    prep. asynchronus vid. rendering and model inference

--- tensorflow-lite/infer.py
+++ tensorflow-lite/infer.py
@@ -2,9 +2,8 @@ import cv2
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-MODEL_PATH = "../models/model.tflite"
+MODEL_PATH = "models/model.tflite"
 
 label_map = ["Pedestrian", "Biker", "Cart", "Skater", "Car", "Bus"]
 classes = label_map
@@ -15,14 +14,13 @@ classes = label_map
 
 COLORS = np.random.randint(0, 255, size=(len(classes), 3), dtype=np.uint8)
 
-def preprocess_image(image_path, input_size, isVideo = False):
-    """Preprocess the input image to feed to the TFLite model"""
-    if not isVideo:
-        img = tf.io.read_file(image_path)
-        img = tf.io.decode_image(img, channels=3)
-    else:
-        img = image_path
+def load_image_from_file(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.io.decode_image(img, channels=3)
+    return img
 
+def preprocess_image(img, input_size):
+    """Preprocess the input image to feed to the TFLite model"""
     img = tf.image.convert_image_dtype(img, tf.uint8)
     original_image = img
     resized_img = tf.image.resize(img, input_size)
@@ -66,17 +64,53 @@ def detect_objects(interpreter, image, threshold):
             results.append(result)
     return results
 
+def run_odt(image, interpreter, threshold=.5):
+    """Run object detection on the input image and draw the detection results"""
+    _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+    
+    preprocessed_image, original_image = preprocess_image(image, (input_height, input_width))
 
-def run_odt_and_draw_results(image_path, interpreter, threshold=0.5, isVideo=False):
+    # Run object detection on the input image
+    results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
+
+    return results
+
+def draw_results(original_image, results):
+    # Plot the detection results on the input image
+    original_image_np = original_image.astype(np.uint8)
+    for obj in results:
+        # Convert the object bounding box from relative coordinates to absolute
+        # coordinates based on the original image resolution
+        ymin, xmin, ymax, xmax = obj['bounding_box']
+        xmin = int(xmin * original_image_np.shape[1])
+        xmax = int(xmax * original_image_np.shape[1])
+        ymin = int(ymin * original_image_np.shape[0])
+        ymax = int(ymax * original_image_np.shape[0])
+
+        # Find the class index of the current object
+        class_id = int(obj['class_id'])
+
+        # Draw the bounding box and label on the image
+        color = [int(c) for c in COLORS[class_id]]
+        cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 2)
+        # Make adjustments to make the label visible for all objects
+        y = ymin - 15 if ymin - 15 > 15 else ymin + 15
+        
+        label = f"{classes[class_id]} [{round(float(obj['score']) * 100)}%]"
+        
+        cv2.putText(original_image_np, label, (xmin, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Return the final image
+    original_uint8 = original_image_np.astype(np.uint8)
+    return original_uint8
+    #return original_image_np
+
+def run_odt_and_draw_results(image, interpreter, threshold=0.5):
     """Run object detection on the input image and draw the detection results"""
     # Load the input shape required by the model
     _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
-
-    # Load the input image and preprocess it
-    if not isVideo:
-        preprocessed_image, original_image = preprocess_image(image_path, (input_height, input_width))
-    else:
-        preprocessed_image, original_image = preprocess_image(image_path, (input_height, input_width), True)
+    
+    preprocessed_image, original_image = preprocess_image(image, (input_height, input_width))
 
     # Run object detection on the input image
     results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
@@ -122,7 +156,7 @@ def inference(model_path: str, path_to_file: str, detection_threshold: str = .3)
 
     # Run inference and draw detection result on the local copy of the original file
     detection_result_image = run_odt_and_draw_results(
-        path_to_file,
+        load_image_from_file(path_to_file),
         interpreter,
         threshold=detection_threshold
     )
@@ -139,29 +173,26 @@ def inference_video(model_path: str, path_to_file: str, detection_threshold: str
     interpreter = tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
     
-    success = True
-    count = 0
+    success, count = True, 0
     while success:
-        success,image = vidcap.read()
-        detection_result_image = run_odt_and_draw_results(
-        image,
-        interpreter,
-        threshold=detection_threshold, 
-        isVideo = True
-        )
-        print(count)
-        count += 1
-        # Show the detection result
-        #img = Image.fromarray(detection_result_image)
-        
-        #img.show()
-        #opencvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        cv2.imshow('object detection', detection_result_image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        success, image = vidcap.read()
+
+        if count % 10 == 0:
+            result = run_odt(
+                image,
+                interpreter,
+                threshold=detection_threshold,
+            )
+
+        detection_result_image = draw_results(image, result)
+
+        cv2.imshow(f"FlyAI: {path_to_file}", detection_result_image)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
             break
 
-    video.release()
+    vidcap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    inference_video(MODEL_PATH, "D:\\Projects\\Datasets\\drone\\video\\nexus\\video1\\video.mp4")
+    # inference(MODEL_PATH, "C:/Users/Nicolas Kolbenschlag/Desktop/quad_video3_00400.jpg")
+    inference_video(MODEL_PATH, "data/video/nexus/video1/video.mp4")
