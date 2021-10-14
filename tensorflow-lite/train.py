041fd480673fef93137de76913a35d60bd386096
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2021-10-14 11:41:50 +0200

    tf-lite model

--- tensorflow-lite/train.py
+++ tensorflow-lite/train.py
@@ -1,7 +1,3 @@
-import numpy as np
-import os
-
-from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
 
@@ -12,10 +8,14 @@ tf.get_logger().setLevel("ERROR")
 from absl import logging
 logging.set_verbosity(logging.ERROR)
 
-spec = model_spec.get("efficientdet_lite0")
-data_loader = object_detector.DataLoader.from_pascal_voc(images_dir="data/sdd/JPEGImages", annotations_dir="data/sdd/Annotations", label_map=["Pedestrian", "Biker", "Cart", "Skater", "Car", "Bus"], max_num_images=10000)
-train_data = data_loader
+def train():
+    spec = model_spec.get("efficientdet_lite0")
+    data_loader = object_detector.DataLoader.from_pascal_voc(images_dir="data/sdd/JPEGImages", annotations_dir="data/sdd/Annotations", label_map=["Pedestrian", "Biker", "Cart", "Skater", "Car", "Bus"], max_num_images=10000)
+    train_data = data_loader
 
-model = object_detector.create(train_data, model_spec=spec, epochs=2, batch_size=16, train_whole_model=True)
+    model = object_detector.create(train_data, model_spec=spec, epochs=2, batch_size=16, train_whole_model=True)
 
-model.export(export_dir="models/tf-lite")
+    model.export(export_dir="models/tf-lite")
+
+if __name__ == "__main__":
+    train()
