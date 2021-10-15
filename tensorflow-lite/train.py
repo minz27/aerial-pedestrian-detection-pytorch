a8d1c5a00414a19b7f8c46eede9e10cfd637bc1b
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2021-10-15 12:56:36 +0200

    upload

--- tensorflow-lite/train.py
+++ tensorflow-lite/train.py
@@ -9,13 +9,11 @@ from absl import logging
 logging.set_verbosity(logging.ERROR)
 
 def train():
-    spec = model_spec.get("efficientdet_lite0")
-    
     labels = ["Pedestrian", "Biker", "Cart", "Skater", "Car", "Bus"]
+    data_loader = object_detector.DataLoader.from_pascal_voc(images_dir="data/sdd/JPEGImages", annotations_dir="data/sdd/Annotations", label_map=labels, max_num_images=10000)
 
-    data_loader = object_detector.DataLoader.from_pascal_voc(images_dir="data/sdd/JPEGImages", annotations_dir="data/sdd/Annotations", label_map=labels, max_num_images=None)
-
-    model = object_detector.create(data_loader, model_spec=spec, epochs=1, batch_size=64, train_whole_model=False)
+    spec = model_spec.get("efficientdet_lite0")
+    model = object_detector.create(data_loader, model_spec=spec, epochs=1, batch_size=8, train_whole_model=False)
     model.export(export_dir="models/tf-lite-02")
 
 if __name__ == "__main__":
